1d6b1abbe60c5dbb3f66365abe04f19974b71a1d
vucris <tranlongvu.info@gmail.com>
2025-12-04 17:51:59 +0700

    update 14-4

--- src/layout/AppFooter.vue
+++
@@ -1,7 +0,0 @@
-<script setup></script>
-
-<template>
-    <div class="layout-footer">
-        <a href="https://primevue.org" target="_blank" rel="noopener noreferrer" class="text-primary font-bold hover:underline">Health Tracking Kids</a>
-    </div>
-</template>
